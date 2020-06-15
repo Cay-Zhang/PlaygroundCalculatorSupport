c44d7064fa5503f87230b5bcfde44ee2dbdddad8
Cay Zhang <zcy20020811@gmail.com>
2020-06-14 22:42:52 -0700

    Angle utilities

--- Sources/PlaygroundCalculatorSupport/Angle.swift
+++ Sources/PlaygroundCalculatorSupport/Angle.swift
@@ -11,5 +11,18 @@ public typealias Angle = Measurement<UnitAngle>
 
 public extension Angle {
     static let straight: Angle = Double.pi.rads
+    
     static func arctan(_ tangent: Double) -> Angle { atan(tangent).rads }
+    
+    static func between<U: Dimension>(_ a⃗: VectorMeasurement2<U>, _ b⃗: VectorMeasurement2<U>) -> Angle {
+        acos((a⃗ * b⃗).baseValue / a⃗.magnitude.baseValue / b⃗.magnitude.baseValue).rads
+    }
+    
+    var sine: Double {
+        sin(self.converted(to: .radians).value)
+    }
+    
+    var cosine: Double {
+        cos(self.converted(to: .radians).value)
+    }
 }
